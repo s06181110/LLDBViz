d0c92739c3f41509979f0375a6c386c8c3e6d0ee
s06181110 <s06181110@gmail.com>
2020-10-03 17:14:01 +0900

    add clang sample

---
+++ target/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add (int a, int b) {
+    int result = a + b;
+    return result;
+}
+
+int main(int argc, const char * argv[]) {
+    char hoge[10] = "hogehoge";
+    int a = 123;
+    int b = 456;
+    int total = 0;
+    total = add(a, b);
+    printf("%d\n", total);
+    return 0;
+}
