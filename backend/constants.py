40baa6d18164290d1733ba3bd51ac0ffd2a811dd
s06181110 <s06181110@gmail.com>
2020-10-13 17:14:31 +0900

    add constants script

---
+++ backend/constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Constants file
+"""
+
+# process
+STEP_INTO = 'STEP_INTO'
+STEP_OVER = 'STEP_OVER'
+STEP_OUT  = 'STEP_OUT'
+CONTINUE  = 'CONTINUE'
+STOP      = 'STOP'
