91f34ee1edd329d33178864a325cd22d6539ed24
s06181110 <s06181110@gmail.com>
2020-10-17 15:42:19 +0900

    Vue init

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  v-app 
+    HelloWorld
+    v-card.red.lighten-4
+      v-card-title Hoge
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
