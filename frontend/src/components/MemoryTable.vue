98a7511fe91741536efee839b4d53de42232dc83
s06181110 <s06181110@gmail.com>
2020-10-20 18:10:05 +0900

    Update Frontend

--- frontend/src/components/MemoryTable.vue
+++ frontend/src/components/MemoryTable.vue
@@ -1,22 +1,84 @@
 <template lang="pug">
-  v-card.mt-10.mx-auto(max-width="500")
+v-container
+  v-card.mx-auto.mt-10( max-width="600" )
     v-card-title Memory Table
-      v-card-text(v-text="memory" style="white-space:pre-wrap; word-wrap:break-word;")
-    v-card-actions.justify-center 
-      v-btn.primary( @click="fetchMemory" ) memory
+    v-card-text
+      v-container
+        v-row
+          v-col.col-12
+            p(v-text="memory" style="white-space:pre-wrap; word-wrap:break-word;")
+          v-col.col-3
+            v-btn( @click="fetchMemory('CONTINUE')" icon small )
+              v-icon mdi-step-forward
+          v-col.col-3
+            v-btn( @click="fetchMemory('STEP_OVER')" icon small )
+              v-icon mdi-debug-step-over 
+          v-col.col-3
+            v-btn( @click="fetchMemory('STEP_INTO')" icon small )
+              v-icon mdi-debug-step-into
+          v-col.col-3
+            v-btn( @click="fetchMemory('fetchMemory')" icon small )
+              v-icon mdi-debug-step-out
+  v-card.mx-auto.mt-10( max-width="600" )
+    v-card-title Debugger
+    v-card-text
+      v-container.align-center
+        v-row
+          v-col.col-12
+            p.text--primary status: {{ status }}
+          v-col.col-6
+            v-select( v-model="breakpointLines" :items="[11,13]" label="breakpoint"  multiple dense )
+          v-col( style="text-align: center;" )
+            v-btn.primary( @click="setBreakpoints" ) set
+          v-col.col-12
+            v-switch( label="show breakpoints" v-model="breakpoints.show")
+            template( v-if="breakpoints.show" )
+              p {{ breakpoints.text }}
+        v-row.justify-center
+          v-btn.mr-8.red( dark @click="launchLLDB" ) launch
+          v-btn.blue-grey( dark @click="stopLLDB" ) stop
 </template>
 
 <script>
 export default {
   name: 'App',
   data: () => ({
-    memory: "None"
+    memory: "None",
+    breakpoints: {
+      show: false,
+      text: '',
+    },
+    breakpointLines: [11],
+    status: 'stop',
   }),
   methods: {
-    fetchMemory () {
-      this.$axios.get('/api/memory').then(res => {
+    fetchMemory (type) {
+      this.$axios.get(`/api/process/${type}`).then(res => {
         this.memory = res.data;
       }).catch(e => console.error(e));
+    },
+    setBreakpoints () {
+      this.$axios.post('/api/breakpoints', this.breakpointLines)
+        .then(res => {
+          this.breakpoints.text = res.data;
+        });
+    },
+    launchLLDB () {
+      this.$axios.get('/api/launch').then(res => {
+        if (res.status === 200) {
+          this.status = 'launch';
+          this.fetchMemory();
+        }
+      });
+      console.log('launch');
+    },
+    stopLLDB () {
+      this.$axios.get('/api/process/STOP').then(res => {
+        if (res.status === 200) {
+          this.status = 'stop';
+          this.fetchMemory();
+        }
+      });
     }
   }
 };
