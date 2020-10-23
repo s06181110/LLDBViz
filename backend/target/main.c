df1f5e5be2ceb09468c3b1a635557d172b08bbf2
s06181110 <s06181110@gmail.com>
2020-10-23 16:46:02 +0900

    Add pointer

--- backend/target/main.c
+++ backend/target/main.c
@@ -8,6 +8,7 @@ int add (int a, int b) {
 int main(int argc, const char * argv[]) {
     char hoge[10] = "hogehoge";
     int a = 123;
+    int *ap = &a;
     int b = 456;
     int total = 0;
     total = add(a, b);
