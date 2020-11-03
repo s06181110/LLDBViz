9dc77b60faf83fac1165a223ae708290f4d20c36
s06181110 <s06181110@gmail.com>
2020-11-03 17:41:04 +0900

    Add function pointer in variables

--- backend/target/main.c
+++ backend/target/main.c
@@ -8,10 +8,14 @@ int add (int a, int b) {
 int main(int argc, const char * argv[]) {
     char hoge[10] = "hogehoge";
     int a = 123;
-    int *ap = &a;
     int b = 456;
     int total = 0;
+    int *ap = &a;
+    int (* functionPointer)(int, int);
+    functionPointer = &add;
     total = add(a, b);
+    int totalByPointer = functionPointer(a, b);
     printf("%d\n", total);
     return 0;
 }
+ 
